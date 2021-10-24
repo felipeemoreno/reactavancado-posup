c564e3040d8536b53296ad3f76f436ca1b8f115a
felipeemoreno <contato@w5server.com.br>
2021-10-24 14:57:29 -0300

    Exercicio Aula 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CustomerForm from './components/CustomerForm';
+
+function App() {
+  return (
+    <div>
+      <CustomerForm />
+    </div>
+  );
+}
+
+export default App;
